a9c9402710b9707b08d15af306d7a65035c6e3ab
Devansh23-maker <Devansh130706@gmail.com>
2026-01-12 20:13:32 +0530

    Add polymorphism example in Python
    Shows method overriding to demonstrate polymorphism.

---
+++ polymorphism.py
@@ -0,0 +1,14 @@
+# Example of Polymorphism in Python
+class Bird:
+    def fly(self):
+        print("Bird can fly")
+
+class Penguin(Bird):
+    def fly(self):
+        print("Penguin cannot fly")
+
+b = Bird()
+p = Penguin()
+
+b.fly()
+p.fly(
